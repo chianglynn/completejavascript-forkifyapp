b747d8057f6b64e1f53b00940d5f9e600517d611
Chiang Lynn <lynnchiangtw@gmail.com>
2021-06-04 22:59:41 +0800

    Refactor by MVC pattern and use fractional library for calculating ingredient quantity

---
+++ src/js/model.js
@@ -0,0 +1,28 @@
+import { async } from 'regenerator-runtime';
+
+export const state = {
+    recipe: {},
+};
+
+export const loadRecipe = async function (id) {
+    try {
+        const response = await fetch(`https://forkify-api.herokuapp.com/api/v2/recipes/${id}`);
+        const data = await response.json();
+
+        if (!response.ok) throw new Error(`${data.message} (${response.status})`)
+
+        const { recipe } = data.data;
+        state.recipe = {
+            id: recipe.id,
+            title: recipe.title,
+            publisher: recipe.publisher,
+            sourceUrl: recipe.source_url,
+            image: recipe.image_url,
+            servings: recipe.servings,
+            cookingTime: recipe.cooking_time,
+            ingredients: recipe.ingredients,
+        };
+    } catch (err) {
+        console.error(err);
+    }
+};
